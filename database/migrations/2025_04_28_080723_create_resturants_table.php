d32c9d7e9fe41c1d9753f600d6e305d841f5a044
MohammedAbdulziz1 <ma9998498@gmail.com>
2025-04-29 22:55:34 +0300

    ini commit

---
+++ database/migrations/2025_04_28_080723_create_resturants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resturants', function (Blueprint $table) {
+            $table->id();
+            $table->string('en_name');
+            $table->string('ar_name');
+            $table->int('phone');
+            $table->int('dail_cod');
+            $table->text('address');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resturants');
+    }
+};
---
+++ database/migrations/2025_04_28_080916_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->int('customer_id');
+            $table->int('driver_id');
+            $table->int('resturant_id');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
