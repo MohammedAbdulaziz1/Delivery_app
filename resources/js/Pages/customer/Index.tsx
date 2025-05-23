a1e26c2f600ce8db7b29b9872a4ac0846921dc31
MohammedAbdulziz1 <ma9998498@gmail.com>
2025-05-23 22:29:46 +0300

    organize react layout

--- resources/js/Pages/customer/Index.tsx
+++ resources/js/Pages/customer/Index.tsx
@@ -1,19 +1,16 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar"
-import { AppSidebar } from "@/Components/SideBar"
-import { Input } from "@/Components/ui/input"
 
-export default function Dashboard() {
+export default function Index() {
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    Customer
                 </h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="Customer" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -27,4 +24,3 @@ export default function Dashboard() {
         </AuthenticatedLayout>
     );
 }
-
